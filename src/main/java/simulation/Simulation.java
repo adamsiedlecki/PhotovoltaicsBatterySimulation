062df8b6dd1fa6676269eb0e70a95c53c855154a
adam <adamsiedlec@wp.pl>
2021-01-10 23:09:17 +0100

    I have added loops to simulate minutes

--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -3,8 +3,8 @@ package simulation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tools.CalcSunriseSunset;
 import pojo.HardwareParams;
+import tools.CalcSunriseSunset;
 import tools.DecimalTimeToMinutes;
 
 import java.time.LocalDateTime;
@@ -13,49 +13,62 @@ import java.time.format.DateTimeFormatter;
 public class Simulation {
 
     private final Logger log = LoggerFactory.getLogger(Simulation.class);
-    private double sunTimeError;
-    private DecimalTimeToMinutes timeToMinutes;
-    private DateTimeFormatter formatter;
+    private final DecimalTimeToMinutes timeToMinutes;
+    private final DateTimeFormatter formatter;
 
     public Simulation() {
-        this.sunTimeError = 0.5;
         this.timeToMinutes = new DecimalTimeToMinutes();
         this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     }
 
-    public int getNumberOfDaysOnBatteryStartingBy(int year, int month, int day, HardwareParams hp, int timezone, double latitude, double longitude ){
+    public int getNumberOfDaysOnBatteryStartingBy(int year, int month, int day, HardwareParams hp, int timezone, double latitude, double longitude) {
         CalcSunriseSunset calcSunriseSunset = new CalcSunriseSunset(timezone, latitude, longitude);
-        double sunrise = calcSunriseSunset.getSunrise(year, month, day);
-        LocalDateTime startTime = LocalDateTime.of(year, month, day,(int)sunrise, timeToMinutes.calcMinutesWithoutHours(sunrise));
+        double sun = calcSunriseSunset.getSunrise(year, month, day);
+        LocalDateTime startTime = LocalDateTime.of(year, month, day, (int) sun, timeToMinutes.calcMinutesWithoutHours(sun));
         LocalDateTime endTime = LocalDateTime.from(startTime);
 
-        log.info("Starting simulation with full battery on "+formatter.format(startTime));
+        log.info("Starting simulation with full battery on " + formatter.format(startTime));
 
         int dayCounter = 0;
         double batteryWh = hp.getBatteryWh();
         double batteryChargingPower = hp.getBatteryChargingPower();
         double minerPowerConsumption = hp.getMinerPowerConsumption();
 
-        while(batteryWh>0){
-            sunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
-            double sunset = calcSunriseSunset.getSunset(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
-            double timeOfTheLight = sunset-sunrise;
-            timeOfTheLight -= 2 * sunTimeError; // it is value set by me to include inefficiency of sunlight just after sunrise and before sunset
-            double timeOfTheDark = 24-timeOfTheLight;
-
-            System.out.println("Sunrise: "+sunrise+" vs time: "+endTime);
-            //System.out.println("light: "+timeOfTheLight+" night:"+timeOfTheDark);
-            // hours of day
-            batteryWh = batteryWh + timeOfTheLight * batteryChargingPower;
-
-            if(batteryWh>hp.getBatteryWh()){
-                batteryWh = hp.getBatteryWh(); // battery cannot be more than 100% full
-            }
-            // hours of night
-            batteryWh = batteryWh - timeOfTheDark * minerPowerConsumption;
-
-
+        while (batteryWh > 0) {
+            double sunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
             endTime = endTime.plusDays(1);
+            double nextSunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+            endTime = endTime.minusDays(1);
+            double sunset = calcSunriseSunset.getSunset(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+            double timeOfTheLight = sunset - sunrise;
+            double lightMinutes = (timeOfTheLight * 60);
+            double darkMinutes = ((24 - sunset + nextSunrise) * 60);
+
+            System.out.println("Sunrise: " + sunrise + " time: " + formatter.format(endTime));
+
+            // minutes of day
+            while (lightMinutes > 0) {
+                batteryWh = batteryWh + (1 / 60d) * batteryChargingPower;
+                if (batteryWh > hp.getBatteryWh()) {
+                    batteryWh = hp.getBatteryWh(); // battery cannot be more than 100% full
+                }
+                lightMinutes--;
+                endTime = endTime.plusMinutes(1);
+
+            }
+            //System.out.println("Sunset: "+sunset+" time: "+formatter.format(endTime));
+
+            // minutes of night
+            while (darkMinutes > 0) {
+                batteryWh = batteryWh - (1 / 60d) * minerPowerConsumption;
+                if (batteryWh <= 0) {
+                    break; // battery is at 0%
+                }
+                darkMinutes--;
+                endTime = endTime.plusMinutes(1);
+
+            }
+
             dayCounter++;
         }
 
