4d4a6b2580e516d57875aab20a574c5cb2e3b9aa
adam <adamsiedlec@wp.pl>
2021-01-10 23:43:22 +0100

    I have provided error counter and packed some code into separated methods

--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -8,6 +8,7 @@ import tools.CalcSunriseSunset;
 import tools.DecimalTimeToMinutes;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class Simulation {
@@ -15,6 +16,7 @@ public class Simulation {
     private final Logger log = LoggerFactory.getLogger(Simulation.class);
     private final DecimalTimeToMinutes timeToMinutes;
     private final DateTimeFormatter formatter;
+    private long TOTAL_ERROR_SECONDS = 0;
 
     public Simulation() {
         this.timeToMinutes = new DecimalTimeToMinutes();
@@ -36,15 +38,17 @@ public class Simulation {
 
         while (batteryWh > 0) {
             double sunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
-            endTime = endTime.plusDays(1);
-            double nextSunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
-            endTime = endTime.minusDays(1);
-            double sunset = calcSunriseSunset.getSunset(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
-            double timeOfTheLight = sunset - sunrise;
-            double lightMinutes = (timeOfTheLight * 60);
-            double darkMinutes = ((24 - sunset + nextSunrise) * 60);
+
+            double darkMinutes = calcDarkMinutes(calcSunriseSunset, endTime);
+            double lightMinutes = calcLightMinutes(calcSunriseSunset, endTime);
 
             System.out.println("Sunrise: " + sunrise + " time: " + formatter.format(endTime));
+
+            // repairing low accuracy error (probably due to calculating decimal to minutes)
+            LocalDateTime copy = LocalDateTime.from(endTime);
+            endTime = endTime.withHour((int) sunrise);
+            endTime = endTime.withMinute(timeToMinutes.calcMinutesWithoutHours(sunrise));
+            TOTAL_ERROR_SECONDS += Math.abs(endTime.toEpochSecond(ZoneOffset.ofHours(1)) - copy.toEpochSecond(ZoneOffset.ofHours(1)));
 
             // minutes of day
             while (lightMinutes > 0) {
@@ -72,8 +76,28 @@ public class Simulation {
             dayCounter++;
         }
 
-        log.info("Start time: "+formatter.format(startTime)+" | end time: "+formatter.format(endTime));
-        log.info("Total days: "+dayCounter);
+        log.info("Start time: " + formatter.format(startTime) + " | end time: " + formatter.format(endTime));
+        log.info("Total days: " + dayCounter);
+        log.info("Total error: " + TOTAL_ERROR_SECONDS / 60 + " minutes");
         return dayCounter;
     }
+
+    private double calcDarkMinutes(CalcSunriseSunset calcSunriseSunset, LocalDateTime endTime) {
+        double sunset = calcSunriseSunset.getSunset(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+
+        endTime = endTime.plusDays(1);
+        double nextSunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+        endTime = endTime.minusDays(1);
+
+        return ((24d - sunset + nextSunrise) * 60);
+    }
+
+    private double calcLightMinutes(CalcSunriseSunset calcSunriseSunset, LocalDateTime endTime) {
+        double sunrise = calcSunriseSunset.getSunrise(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+        double sunset = calcSunriseSunset.getSunset(endTime.getYear(), endTime.getMonthValue(), endTime.getDayOfMonth());
+
+        double timeOfTheLight = sunset - sunrise;
+        return (timeOfTheLight * 60);
+    }
+
 }
