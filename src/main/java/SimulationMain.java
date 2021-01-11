5253cd18a3aae2c212ec510808ec63fa690582a9
adam <adamsiedlec@wp.pl>
2021-01-11 11:02:29 +0100

    I have updated console screenshot

--- src/main/java/SimulationMain.java
+++ src/main/java/SimulationMain.java
@@ -8,7 +8,7 @@ public class SimulationMain {
     private static final Logger log = LoggerFactory.getLogger(SimulationMain.class);
 
     public static void main(String[] args) {
-        log.error("Starting app");
+        log.info("Starting app");
 
         Simulation simulation = new Simulation();
         simulation.getNumberOfDaysOnBatteryStartingBy(2021, 2, 13, new HardwareParams(), 1, 52, 21);
